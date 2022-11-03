75e4b2c3249699973fbe5ff774e49095bbdfb3b7
Jakub Turek <kubo.bubo@interia.pl>
2022-11-03 22:25:03 +0100

    finished

---
+++ src/fetchCountries.js
@@ -0,0 +1,51 @@
+const FETCH_URL = 'https://restcountries.com/v3.1/name/';
+import Notiflix from 'notiflix';
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+export function fetchCountries(name) {
+  fetch(FETCH_URL + name + '?fields=name,capital,population,flags,languages')
+    .then(response => response.json())
+    .then(countries => {
+      console.log(countries);
+      let countryMarkup = [];
+      countries.forEach(country => {
+        console.log(country.name.common);
+        countryMarkup += `<li class="country"><img class="country-svg" src="${country.flags.svg}" height="18px"></img>${country.name.common}</li>`;
+      });
+
+      if(countries.length > 10) {
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        return
+    }
+    if(countries.length === 1) {
+        countries.forEach(country => {
+            const values = Object.values(country.languages);
+        countryInfo.innerHTML = `<p class="country-category">Capital: <span class="country-details"> ${country.capital}</span></p>
+        <p class="country-category">Population: <span class="country-details">${numFormatter(country.population)}</span></p>
+        <p class="country-category">Languages: <span class="country-details"> ${values}</span></p> `
+        countryInfo.style.transform = "translate(0%)"
+    
+    })
+    }
+    if(countries.length !== 1) {
+        countryInfo.style.transform = "translate(-150%)";
+        countryInfo.innerHTML = "";
+    }
+      countryList.innerHTML = countryMarkup;
+      
+    }).catch((error) => {
+
+        Notiflix.Notify.failure("Oops, there is no country with that name");
+        
+    });
+}
+
+function numFormatter(num) {
+    if(num > 999 && num < 1000000){
+        return (num/1000).toFixed(1) + ' K'; // convert to K for number from > 1000 < 1 million 
+    }else if(num > 1000000){
+        return (num/1000000).toFixed(1) + ' Mln'; // convert to M for number from > 1 million 
+    }else if(num < 900){
+        return num; // if value < 1000, nothing to do
+    }}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 
+const inputSearchBox = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector(".country-info")
 const DEBOUNCE_DELAY = 300;
+
+
+inputSearchBox.addEventListener('input', debounce(() => {
+    let insertedCountryNames = inputSearchBox.value.trim();
+
+    if(!insertedCountryNames) {
+        console.log("no country name provided")
+        countryList.innerHTML = "";
+        countryInfo.innerHTML = "";
+        return
+    }
+    fetchCountries(insertedCountryNames)
+
+}, DEBOUNCE_DELAY));
